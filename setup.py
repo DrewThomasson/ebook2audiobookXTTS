2ea32f7de37003920313373e4723eace8c71e9ca
ROBERT MCDOWELL <2649072+ROBERT-MCDOWELL@users.noreply.github.com>
2024-10-10 18:25:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,48 @@
-import subprocess
-from setuptools import setup, find_packages, Command
+import os
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
-class InstallPackages(Command):
-    description = 'Install dependencies and download additional resources'
-    user_options = []
 
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        subprocess.check_call(['pip', 'install', '-r', 'requirements.txt'])
-        subprocess.check_call(['python', '-m', 'unidic', 'download'])
-        subprocess.check_call(['python', '-m', 'nltk.downloader', 'punkt'])
+cwd = os.path.dirname(os.path.abspath(__file__))
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
-
-# Read requirements.txt
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        os.system('python -m unidic download')
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        os.system('python -m unidic download')
 
 setup(
-    name="ebook2audiobookXTTS",
-    version="1.2",
+    name='ebook2audiobookXTTS',
+    version='1.2.0',
     author="Drew Thomasson",
-    description="Convert eBooks to audiobooks with chapters and metadata.",
+    description="Convert eBooks to audiobooks with chapters and metadata",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/DrewThomasson/ebook2audiobookXTTS",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'ebook2audiobook=ebook2audiobook:main',
-        ],
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    cmdclass={
-        'install': InstallPackages,
-    },
-    python_requires='>=3.10',
+    include_package_data=True,
     install_requires=requirements,
-    keywords='ebook, audiobook, '
-             'TTS-engine, python'
+    entry_points={
+        "console_scripts": [
+            "ebook2audiobook = app:main",
+        ],
+    },
+    keywords='ebook, audiobook, TTS-engine, python',
 )
