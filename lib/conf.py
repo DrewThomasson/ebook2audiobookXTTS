3e819a3efa06f909c7488bebc883d6d9dfe484b6
unknown <admin@radsl.net>
2024-11-28 04:35:05 -0800

    double quotes to simple quotes normalization

--- lib/conf.py
+++ lib/conf.py
@@ -1,40 +1,40 @@
 import os
 
-NATIVE = "native"
-DOCKER_UTILS = "docker_utils"
-FULL_DOCKER = "full_docker"
+NATIVE = 'native'
+DOCKER_UTILS = 'docker_utils'
+FULL_DOCKER = 'full_docker'
 
-version = "2.0.0"
+version = '2.0.0'
 min_python_version = (3,10)
 max_python_version = (3,12)
 
-requirements_file = os.path.abspath(os.path.join(".","requirements.txt"))
+requirements_file = os.path.abspath(os.path.join('.','requirements.txt'))
 
 docker_utils_image = 'utils'
 gradio_interface_port = 7860
 gradio_shared_expire = 72 # hours
 concurrency_limit = 16 # or None for unlimited
 
-python_env_dir = os.path.abspath(os.path.join(".","python_env"))
-models_dir = os.path.abspath(os.path.join(".","models"))
-ebooks_dir = os.path.abspath(os.path.join(".","ebooks"))
-processes_dir = os.path.abspath(os.path.join(".","tmp"))
-audiobooks_gradio_dir = os.path.abspath(os.path.join(".","audiobooks","gui","gradio"))
-audiobooks_host_dir = os.path.abspath(os.path.join(".","audiobooks","gui","host"))
-audiobooks_cli_dir = os.path.abspath(os.path.join(".","audiobooks","cli"))
+python_env_dir = os.path.abspath(os.path.join('.','python_env'))
+models_dir = os.path.abspath(os.path.join('.','models'))
+ebooks_dir = os.path.abspath(os.path.join('.','ebooks'))
+processes_dir = os.path.abspath(os.path.join('.','tmp'))
+audiobooks_gradio_dir = os.path.abspath(os.path.join('.','audiobooks','gui','gradio'))
+audiobooks_host_dir = os.path.abspath(os.path.join('.','audiobooks','gui','host'))
+audiobooks_cli_dir = os.path.abspath(os.path.join('.','audiobooks','cli'))
 
 # <<<<<<< HEAD
-os.environ["CALIBRE_TEMP_DIR"] = processes_dir
-os.environ["CALIBRE_CACHE_DIRECTORY"] = processes_dir
-os.environ["CALIBRE_NO_NATIVE_FILEDIALOGS"] = "1"
-os.environ["DO_NOT_TRACK"] = "true"
-os.environ["HUGGINGFACE_HUB_CACHE"] = models_dir
-os.environ["HF_HOME"] = models_dir
-os.environ["HF_DATASETS_CACHE"] = models_dir
-os.environ["HF_TOKEN_PATH"] = os.path.join(os.path.expanduser("~"), ".huggingface_token")
-os.environ["TTS_CACHE"] = models_dir
-os.environ["TORCH_HOME"] = models_dir
-os.environ["XDG_CACHE_HOME"] = models_dir
+os.environ['CALIBRE_TEMP_DIR'] = processes_dir
+os.environ['CALIBRE_CACHE_DIRECTORY'] = processes_dir
+os.environ['CALIBRE_NO_NATIVE_FILEDIALOGS'] = '1'
+os.environ['DO_NOT_TRACK'] = 'true'
+os.environ['HUGGINGFACE_HUB_CACHE'] = models_dir
+os.environ['HF_HOME'] = models_dir
+os.environ['HF_DATASETS_CACHE'] = models_dir
+os.environ['HF_TOKEN_PATH'] = os.path.join(os.path.expanduser('~'), '.huggingface_token')
+os.environ['TTS_CACHE'] = models_dir
+os.environ['TORCH_HOME'] = models_dir
+os.environ['XDG_CACHE_HOME'] = models_dir
 
 models = {
     "xtts": {
@@ -52,8 +52,8 @@ models = {
 }
 
 ebook_formats = ['.epub', '.mobi', '.azw3', 'fb2', 'lrf', 'rb', 'snb', 'tcr', '.pdf', '.txt', '.rtf', '.docx', '.html', '.odt', '.azw']
-audiobook_format = "m4b"
-audio_proc_format = "wav" # only "wav" is valid for now
+audiobook_format = 'm4b'
+audio_proc_format = 'wav' # only 'wav' is valid for now
 
 xtts_fine_tuned_voice_actors = {
     "David Attenborough": {
